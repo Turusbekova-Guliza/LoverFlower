6c4261244c966bc213352cdcd1677ce43406755c
Turusbekova-Guliza <134058547+Turusbekova-Guliza@users.noreply.github.com>
2024-05-04 15:31:02 +0600

    first sync

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,33 @@ import React from "react";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
+import About from "./pages/about/About";
+import Basket from "./pages/basket/Basket";
+import CardsProduct from "./pages/cardsProduct/CardProduct";
+import Catalog from "./pages/catalog/Catalog";
+import DelPay from "./pages/delPay";
+import FAQ from "./pages/faq/FAQ";
+import Home from "./pages/home/Home";
+import Mistake from "./pages/mistake/Mistake";
+import NotFound from "./pages/notFound/NotFound";
+import Order from "./pages/order/Order";
+import Success from "./pages/success/Success";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="basket" element={<Basket />} />
+        <Route path="cardsProduct" element={<CardsProduct />} />
+        <Route path="catalog" element={<Catalog />} />
+        <Route path="delPay" element={<DelPay />} />
+        <Route path="faq" element={<FAQ />} />
+        <Route path="mistake" element={<Mistake />} />
+        <Route path="notFound" element={<NotFound />} />
+        <Route path="order" element={<Order />} />
+        <Route path="success" element={<Success />} />
       </Route>
     </Routes>
   );
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Header() {
   return (
-    <div>Header</div>
+    <div>
+      <Link to='/'>home</Link>
+      <Link to='about'>about</Link>
+      <Link to='basket'>basket</Link>
+      <Link to='cardsPrduct'>cards product</Link>
+      <Link to='catalog'>catalog</Link>
+      <Link to='delPay'>delivery payment</Link>
+      <Link to='faq'>faq</Link>
+      <Link to='mistake'>mistake</Link>
+      <Link to='notFound'>not found 404</Link>
+      <Link to='order'>order</Link>
+      <Link to='success'>success</Link>
+    </div>
   )
 }
 
