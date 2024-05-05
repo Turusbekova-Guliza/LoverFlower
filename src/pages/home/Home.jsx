8ae6531462ebc2d6c6347a872d9d81e02a6f5459
eldar Ruslanov <ruslanoveldar16@gmail.com>
2024-05-05 13:33:05 +0600

    hero section

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import './Home.scss'
+import Hero from '../../components/Hero/Hero'
 
 function Home() {
   return (
-    <div className='home-container'>Home</div>
+    <div className='home-container'>
+      <Hero/>
+    </div>
   )
 }
 
